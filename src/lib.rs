5c9cfba3121020267c7de6bb2152617f13c1667b
SauceBoss1 <dtvsworld@gmail.com>
2024-04-05 16:25:37 -0700

    next up: add delete

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 mod tree;
-#[allow(dead_code)]
 #[derive(Clone, Debug)]
 struct Node<T: PartialOrd> {
     val: T,
@@ -14,11 +13,60 @@ pub struct Tree<T: PartialOrd> {
 
 #[cfg(test)]
 mod test {
-    use super::Node;
+    use super::{Node, Tree};
 
     #[test]
     fn new_node() {
         let node = Node::new(10);
-        println!("{}", node.val);
+        println!("{:?}", node);
+    }
+
+    #[test]
+    fn new_tree() {
+        let tree: Tree<i32> = Tree::new();
+        println!("Tree init: {:#?}", tree);
+    }
+
+    #[test]
+    fn insert_tree() {
+        let mut tree = Tree::new();
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(7);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(7);
+        tree.insert(8);
+        println!("{tree}");
+        let t2 = tree.clone();
+        println!("{t2}");
+    }
+
+    #[test]
+    fn str_trees() {
+        let mut str_tree: Tree<String> = Tree::new();
+        str_tree
+            .insert("Hello ".to_string())
+            .insert("World!".to_string())
+            .insert("It is my Birthday!".to_string())
+            .insert("I am old now.".to_string());
+
+        println!("{str_tree}");
+    }
+
+    #[test]
+    fn exist_test() {
+        let mut t: Tree<i32> = Tree::new();
+        t.insert(5)
+            .insert(3)
+            .insert(7)
+            .insert(8)
+            .insert(9)
+            .insert(1)
+            .insert(3);
+
+        assert_eq!(t.does_exist(1), true);
+        assert_eq!(t.does_exist(100), false);
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,7 +1,7 @@
-use crate::Tree;
+use crate::{Node, Tree};
+use std::fmt::{self, Display, Formatter};
 
-use super::Node;
-
+// Node implementation
 impl<T: PartialOrd> Node<T> {
     pub fn new(val_in: T) -> Box<Node<T>> {
         Box::new(Node {
@@ -13,7 +13,77 @@ impl<T: PartialOrd> Node<T> {
 }
 
 impl<T: PartialOrd> Tree<T> {
+    // creating a new Tree
     pub fn new() -> Self {
         Tree { root: None }
+    }
+
+    // Inserting Nodes into Tree
+    pub fn insert(&mut self, val: T) -> &mut Self {
+        if self.root.is_none() {
+            self.root = Some(Node::new(val));
+            return self;
+        }
+
+        let mut curr_node = &mut self.root;
+        while let Some(ref mut node) = curr_node {
+            if val < node.val {
+                if node.left_child.is_none() {
+                    node.left_child = Some(Node::new(val));
+                    return self;
+                } else {
+                    curr_node = &mut node.left_child;
+                }
+            } else {
+                if node.right_child.is_none() {
+                    node.right_child = Some(Node::new(val));
+                    return self;
+                } else {
+                    curr_node = &mut node.right_child;
+                }
+            }
+        }
+
+        return self;
+    }
+
+    pub fn does_exist(&self, val: T) -> bool {
+        self.search(val).is_some()
+    }
+
+    fn search(&self, val: T) -> Option<&Node<T>> {
+        return self.search_recurse(&self.root, val);
+    }
+
+    fn search_recurse<'a>(&self, node: &'a Option<Box<Node<T>>>, val: T) -> Option<&'a Node<T>> {
+        match node {
+            Some(ref n) if val == n.val => Some(n),
+            Some(ref n) if val < n.val => self.search_recurse(&n.left_child, val),
+            Some(ref n) => self.search_recurse(&n.right_child, val),
+            _ => None,
+        }
+    }
+}
+
+impl<T: PartialOrd + std::fmt::Debug> Tree<T> {
+    fn build_inorder_str(&self, node: &Option<Box<Node<T>>>, buffer: &mut String) {
+        if let Some(ref n) = node {
+            self.build_inorder_str(&n.left_child, buffer);
+
+            if !buffer.is_empty() {
+                buffer.push_str(", ");
+            }
+            buffer.push_str(&format!("{:?}", n.val));
+
+            self.build_inorder_str(&n.right_child, buffer);
+        }
+    }
+}
+
+impl<T: PartialOrd + std::fmt::Debug> Display for Tree<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut out_str = String::new();
+        self.build_inorder_str(&self.root, &mut out_str);
+        return write!(f, "{}", out_str);
     }
 }
